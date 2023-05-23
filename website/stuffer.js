f53a8025317871c7fa3f6e51f4811d1f2e8ecb78
Kajetan Owczarek <flash3ch@gmail.com>
2023-05-23 22:45:06 +0200

    Finalizing, adding some blocking scrripts

---
+++ website/stuffer.js
@@ -0,0 +1,28 @@
+const arrr = [...[...Array(1_000_000)].map((v,i) => Math.sin(i)*Math.cos(i))]
+
+document.addEventListener('DOMContentLoaded', ev => {
+    document.getElementById('luma-receiver').replaceChildren(
+        ... Array(25).fill(0).map((v, i) => {
+            const e = document.createElement('div')
+            const luma = (i+1) * 10;
+            e.classList.add("luma-bg-"+luma, "fix-text")
+            e.innerText = luma
+            return e
+        })
+    )
+
+    document.getElementById('grid-receiver').replaceChildren(
+        ... Array(100).fill(0).map((_, row) => {
+            const rowElement = document.createElement('div')
+            rowElement.replaceChildren(
+                ... Array(100).fill(0).map((_, col) => {
+                    const cellElem = document.createElement('div')
+                    cellElem.classList.add('col-'+col, 'row-'+row)
+                    cellElem.innerText = `Row ${row}, Column ${col}`
+                    return cellElem
+                })
+            )
+            return rowElement
+        })
+    )
+})
