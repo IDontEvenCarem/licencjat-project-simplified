9b5fe93d63e17019daef463fe2444733d009bc79
Kajetan Owczarek <flash3ch@gmail.com>
2023-07-01 10:25:39 +0200

    Changed webp to be marked and using cwebp

--- scripts/convert_to_webp.js
+++ scripts/convert_to_webp.js
@@ -2,13 +2,16 @@ const IMAGE_EXTENTIONS = ['.png', '.jpg', '.jpeg', '.tiff', '.bmp']
 
 const childProcess = require('child_process')
 const fs = require('fs')
+const path = require('path')
+
+const QUALITY = process.argv[2] || 20
 
 for(const file of fs.readdirSync('.')) {
     if (!IMAGE_EXTENTIONS.map(ext => file.endsWith(ext)).some(Boolean)) {
         continue;
     }
-    const newName = file.replace(/\.\w+$/, '.webp')
-    const res = childProcess.spawnSync('ffmpeg', ['-i', file, newName])
+    const newName = `${path.parse(file).name}-${QUALITY}.webp`
+    const res = childProcess.spawnSync('cwebp', [file, '-q', QUALITY, '-o', newName])
     const msg = res.status === 0 ? "OK" : ("ERR: " + res.stderr.toString())
     console.log(`${file} => ${newName} ${msg}`);
 }
