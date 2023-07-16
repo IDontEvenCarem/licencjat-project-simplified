d28865777d22bc71a97e7ae0c0b5011518b6c927
Kajetan Owczarek <flash3ch@gmail.com>
2023-07-16 19:58:30 +0200

    Getting rid of vue and lodash

--- website/index.js
+++ website/index.js
@@ -1,78 +1,68 @@
 document.addEventListener("DOMContentLoaded", ev => {
-    const {createApp} = Vue;
-
     // top news carousel
-    createApp({
-        data() {
-            return {
-                topNews: []
-            }
-        },
-        mounted() {
-            axios.get('/api/top-news').then(response => {
-                this.topNews = response.data;
-                requestAnimationFrame(() => {
-                    $('#top-carousel').slick({dots: true, speed: 1000})
-                })
-            })
-        },
-        template: `
-            <div v-for="neu in topNews">
-                <div class="slider-article">
-                    <figure class="slider-article--image is-square image 256x256">
-                        <picture>
-                            <source :srcset="neu.img.replace('WIDTH', 368)" media="(max-width: 416px), (min-width: 770px) and (max-width: 1366px)" />
-                            <source :srcset="neu.img.replace('WIDTH', 432)" media="(min-width: 417px) and (max-width: 480px), (min-width: 1367px)" />
-                            <img :src="neu.img.replace('WIDTH', 720)">
-                        </picture>
-                    </figure>
-                    <p class="slider-article--title">
-                        {{neu.title}}
-                    </p>
-                    <p class="slider-article--text">
-                        {{neu.text}}
-                    </p>
-                </div>
-            </div>
-        `
-    }).mount('#top-carousel')
-
-    // post list
-    createApp({
-        data() {
-            return {
-                posts: []
-            }
-        },
-        mounted() {
-            axios.get("/api/newsHeaders").then(response => {
-                this.posts = response.data;
-            })
-        },
-        /* TODO: srcset/sizes needs to reflect the small device width, when the images grow, because they are not in a 3 column grid */
-        template: `
-            <div v-for="post in posts" class="column is-one-third">
-                <div class="card has-shadow post-column">
-                    <div class="card-image">
-                        <figure class="image is-square">
+    fetch('/api/top-news')
+        .then(res => res.json())
+        .then(json => {
+            const els = json.map(neu => {
+                const root = document.createElement('div')
+                root.innerHTML = `
+                    <div class="slider-article">
+                        <figure class="slider-article--image is-square image 256x256">
                             <picture>
-                                <source :srcset="post.image.replace('WIDTH', 368)" media="(max-width: 416px), (min-width: 770px) and (max-width: 1366px)" />
-                                <source :srcset="post.image.replace('WIDTH', 432)" media="(min-width: 417px) and (max-width: 480px), (min-width: 1367px)" />
-                                <img :src="post.image.replace('WIDTH', 720)">
+                                <source srcset="${neu.img.replace('WIDTH', 368)}" media="(max-width: 416px), (min-width: 770px) and (max-width: 1366px)" />
+                                <source srcset="${neu.img.replace('WIDTH', 432)}" media="(min-width: 417px) and (max-width: 480px), (min-width: 1367px)" />
+                                <img src="${neu.img.replace('WIDTH', 720)}">
                             </picture>
                         </figure>
+                        <p class="slider-article--title">
+                            ${neu.title}
+                        </p>
+                        <p class="slider-article--text">
+                            ${neu.text}
+                        </p>
                     </div>
-                    <div class="card-content">
-                        <p class="subtitle card-title">{{post.title}}</p>
-                        <p>{{post.first}}</p>
+                `
+                return root;
+            })
+            const topCarousel = document.getElementById('top-carousel')
+            topCarousel.replaceChildren.apply(topCarousel, els)
+        })
+        .then(_ => {
+            $('#top-carousel').slick({dots: true, speed: 1000})
+        })
+
+    // post list
+    fetch('/api/newsHeaders')
+        .then(response => response.json())
+        .then(json => {
+            const postEls = json.map(post => {
+                const root = document.createElement('div')
+                root.classList.add('column', 'is-one-third')
+                root.innerHTML = `
+                    <div class="card has-shadow post-column">
+                        <div class="card-image">
+                            <figure class="image is-square">
+                                <picture>
+                                    <source srcset="${post.image.replace('WIDTH', 368)}" media="(max-width: 416px), (min-width: 770px) and (max-width: 1366px)" />
+                                    <source srcset="${post.image.replace('WIDTH', 432)}" media="(min-width: 417px) and (max-width: 480px), (min-width: 1367px)" />
+                                    <img src="${post.image.replace('WIDTH', 720)}">
+                                </picture>
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <p class="subtitle card-title">${post.title}</p>
+                            <p>${post.first}</p>
+                        </div>
                     </div>
-                </div>
-            </div>
-        `
-    }).mount("#post-columns");
+                `
+                return root
+            })
+            const postColumns = document.getElementById('post-columns')
+            postColumns.replaceChildren.apply(postColumns, postEls)
+        })
 
     // load the content-spec asynchronously
-    (async () => {
+    ;(async () => {
         const streamDoc = document.implementation.createHTMLDocument()
         const decoder = new TextDecoderStream('utf-8')
         const dlStream = await fetch('content-spec.html')
