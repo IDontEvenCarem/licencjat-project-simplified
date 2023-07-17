f5850ba12f9b84fd7c0a6a3d806d8c698999a430
Kajetan Owczarek <flash3ch@gmail.com>
2023-07-17 10:42:57 +0200

    Lil inlining and merging of files

--- website/alldefer.js
+++ website/alldefer.js
@@ -1,3 +1,33 @@
+import("https://cdn.jsdelivr.net/npm/chart.js").then(() => {
+    // recent poll
+    fetch('/api/lastPoll')
+        .then(res => res.json())
+        .then(json => {
+            const chartEl = document.getElementById('recentPollChart')
+
+            document.getElementById('recentPollTitle').innerText = json.temat
+    
+            new Chart(chartEl, {
+                type: 'bar',
+                data: {
+                    labels: json.opcje_odpowiedzi,
+                    datasets: [{
+                        label: "Ilość odpowiedzi",
+                        data: Object.values(json.wyniki),
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            })
+        })
+})
+
 const arrr = Array(1_000_000).fill(0).map((v, i) => Math.sin(i)*Math.cos(i))
 
 document.addEventListener('DOMContentLoaded', ev => {
